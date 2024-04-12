0175a5f7c2ec67ea3f97f59deaf2bbfe41cdca15
guodong <32507511+CollectBugs@users.noreply.github.com>
2024-04-12 15:07:45 +0800

    Update TableStyleConfig.java

--- src/main/java/com/gz/easyhtml/core/pojo/TableStyleConfig.java
+++ src/main/java/com/gz/easyhtml/core/pojo/TableStyleConfig.java
@@ -12,47 +12,5 @@ import lombok.experimental.SuperBuilder;
 @Builder
 public class TableStyleConfig {
 
-    //标题样式
-    private TitleStyle titleStyle;
-    //列样式
-    private ColumnStyle columnStyle;
-    //单元格样式
-    private RowStyle rowStyle;
-
-    /**
-     * 边框
-     */
-    @Data
-    @SuperBuilder
-    public static class TitleStyle {
-        //合并单元格
-        private Integer columnColspanNum;
-        //文本位置
-        private PositionEnum textPosition;
-
-    }
-    /**
-     * 边框
-     */
-    @Data
-    @SuperBuilder
-    public static class ColumnStyle extends StyleBase {
-
-        //背景色
-        private String backGroundColor;
-        //字体颜色
-        private String textColor;
-
-    }
-    /**
-     * 边框
-     */
-    @Data
-    @SuperBuilder
-    public static class RowStyle extends StyleBase {
-        //字体类型
-        private String fontType;
-        //字体大小
-        private String fontSize;
-    }
+  
 }
